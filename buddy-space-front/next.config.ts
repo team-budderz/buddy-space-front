e109d873fc20479663b90f3bb4a38d24ebd9eaab
haneul02 <redlight02@naver.com>
2025-07-07 00:05:06 +0900

    모듈 설치

--- buddy-space-front/next.config.ts
+++ buddy-space-front/next.config.ts
@@ -1,7 +1,35 @@
-import type { NextConfig } from "next";
+import path from 'path'
+import type { NextConfig } from 'next'
+import type { Configuration as WebpackConfig } from 'webpack'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/oauth2/authorization/:provider',
+        destination: 'http://localhost:8080/oauth2/authorization/:provider',
+      },
+      {
+        source: '/login/oauth2/code/:provider',
+        destination: 'http://localhost:8080/login/oauth2/code/:provider',
+      },
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      },
+    ]
+  },
+  webpack(config: WebpackConfig, options) {
+    config.resolve = config.resolve || {}
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
